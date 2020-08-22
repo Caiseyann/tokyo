e06a0c12d0e0ca63c93e7a070c2b99c5ea03770f
caiseyann <caiseyann4@gmail.com>
2020-08-22 12:40:17 +0300

    first quote added

--- tokyo/src/app/quote/quote.component.ts
+++ tokyo/src/app/quote/quote.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+ Quotes = [
+   new Quote(1, '-Brenda Mwangi', '"Be happy, Never gloomy"', new Date(2020, 8, 22))
+ ]
   constructor() { }
 
   ngOnInit(): void {
