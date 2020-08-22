28119b85c37a70978a6a5ce09aa38a56a6235477
caiseyann <caiseyann4@gmail.com>
2020-08-22 13:03:10 +0300

    new quotes added

--- tokyo/src/app/quote/quote.component.ts
+++ tokyo/src/app/quote/quote.component.ts
@@ -10,7 +10,8 @@ export class QuoteComponent implements OnInit {
  Quotes = [
    new Quote(1, '-Brenda Mwangi', '"Be happy, Never gloomy"', new Date(2020, 8, 22)),
    new Quote(2, '-Mother Teresa', '"If you judge people, you have no time to love them."',new Date(2015, 8, 22)),
-   new Quote
+   new Quote(3,'- Buddha', '"All that we are is the result of what we have thought"',new Date(2010, 1, 12) ),
+   
  ]
   constructor() { }
 
